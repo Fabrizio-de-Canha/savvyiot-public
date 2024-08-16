8b69bfad09728851ae11f70f72133fa47ad0f541
Fabrizio-de-Canha <fabriziodecanha@gmail.com>
2024-08-16 17:33:35 +0200

    added v2 of product Counter Firmware

---
+++ firmware/productcounterv2/main.py
@@ -0,0 +1,102 @@
+import machine  # type: ignore
+import time
+import utime # type: ignore
+from boot import do_connect, set_time, update_time
+from mqtt import connect_mqtt, send_payload, send_health_check
+import network # type: ignore
+from ota import OTAUpdater
+from secret import firmware_url
+import os
+import json
+
+led_pin = machine.Pin(13, machine.Pin.OUT)
+relay_1 = machine.Pin(33, machine.Pin.OUT)
+relay_2 = machine.Pin(32, machine.Pin.OUT)
+digital_input = machine.Pin(16, machine.Pin.IN)
+
+relay_1_input = machine.Pin(34, machine.Pin.IN)
+relay_2_input = machine.Pin(35, machine.Pin.IN)
+
+## WIFI
+wifi_client = network.WLAN(network.STA_IF)
+do_connect(wifi_client)
+
+##Check for firmware update
+if wifi_client.isconnected():
+    set_time()
+    ota_updater = OTAUpdater(wifi_client, firmware_url, ['main.py', 'boot.py', 'mqtt.py'])
+    ota_updater.download_and_install_update_if_available()
+
+
+##MQTT
+mqtt_client = connect_mqtt()
+
+firmware_version = 0
+
+if 'version.json' in os.listdir():    
+    with open('version.json') as f:
+        firmware_version = int(json.load(f)['version'])
+
+mqtt_client = send_health_check(wifi_client, mqtt_client, firmware_version)
+
+message_interval = 5
+
+times = []
+timestamps = []
+
+lastTimeUpdate = 946684800 + utime.time()
+lastHealthcheck = 946684800 + utime.time()
+lastTicks = 0
+currentTicks = 0
+
+lastDigitalReading = -1
+
+while True:
+    timestamp = 946684800 + utime.time()
+
+    ## Read digital pin
+    currentDigitalReading = digital_input.value()
+
+    if timestamp - lastTimeUpdate > 86400:
+        # If it's been a day since last time update
+        update_time(wifi_client)
+        lastTimeUpdate = timestamp
+
+    if timestamp - lastHealthcheck > 1800:
+        # send health check every hour
+        mqtt_client = send_health_check(wifi_client, mqtt_client)
+        lastHealthcheck = timestamp
+
+    if(relay_1_input.value() == 1):
+        relay_1.value(1)
+    else:
+        relay_1.value(0)
+
+    if(relay_2_input.value() == 1):
+        relay_2.value(1)
+    else:
+        relay_2.value(0)
+
+    if (currentDigitalReading == 0 and lastDigitalReading == 1):
+        led_pin.value(0)
+        currentTicks = utime.ticks_ms()
+        if lastTicks > 0:
+            ticksDifference = time.ticks_diff(currentTicks, lastTicks)
+
+            if ticksDifference > 10:
+                times.append(ticksDifference)
+                timestamps.append(timestamp)
+
+                ## Send the data here
+                print(times)
+                print(timestamps)
+
+                mqtt_client, times, timestamps = send_payload(wifi_client, mqtt_client, times, timestamps)
+        
+        lastTicks = currentTicks
+    
+    if (currentDigitalReading == 1 and lastDigitalReading == 0):
+        led_pin.value(1)
+
+    lastDigitalReading = currentDigitalReading
+
---
+++ firmware/productcounterv2/mqtt.py
@@ -0,0 +1,109 @@
+from umqtt.simple import MQTTClient  # type: ignore
+import machine  # type: ignore
+import ubinascii  # type: ignore
+import time
+from boot import do_connect, set_time
+import utime # type: ignore
+from secret import *
+import os
+
+led_pin = machine.Pin(13, machine.Pin.OUT)
+
+macAddress = ubinascii.hexlify(machine.unique_id()).decode()
+
+## OUT
+mqtt_data_topic = f'{mqtt_ClientID}/productcounter/{macAddress}/data'
+mqtt_health_topic = f'{mqtt_ClientID}/productcounter/{macAddress}/status'
+
+## IN
+mqtt_control_topic = f'{mqtt_ClientID}/productcounter/{macAddress}/control'
+
+def recieve_mqtt(topic, msg):
+    print('received message %s on topic %s' % (msg, topic))
+
+def connect_mqtt():
+    client = MQTTClient(f'{mqtt_ClientID}_{macAddress}', mqtt_server, 1883, mqtt_username, mqtt_password)
+
+    ## Retry connections here
+    ## Retry 10 times max
+    for i in range(5):
+        print(f'connecting to mqtt: try {i + 1}...')
+        try:
+            client.connect()
+            for j in range(5):  
+                led_pin.value(0)
+                time.sleep(0.1)
+                led_pin.value(1)
+                time.sleep(0.1)
+            print('connected to mqtt server')
+            return client
+        except OSError:
+            for j in range(5):  
+                led_pin.value(0)
+                time.sleep(0.1)
+                led_pin.value(1)
+                time.sleep(0.1)
+
+    print('failed to connect to mqtt server')
+
+    return client
+
+def send_health_check(wifi_client, mqtt_client, firmware_version):
+    ## Get firmware version
+
+    if not wifi_client.isconnected():
+        do_connect(wifi_client)
+        if wifi_client.isconnected():
+            set_time()
+
+    if wifi_client.isconnected():
+        rssi = wifi_client.status('rssi')
+        timestamp = 946684800 + utime.time()
+
+        try:
+            mqtt_client.publish(mqtt_health_topic, f'{{"timestamp":{timestamp},"rssi":{rssi},"firmware_version":{firmware_version}}}')
+        except OSError:
+                mqtt_client = connect_mqtt()
+                try:
+                    mqtt_client.publish(mqtt_health_topic, f'{{"timestamp":{timestamp},"rssi":{rssi},"firmware_version":{firmware_version}}}')
+                except OSError:
+                    return
+    
+    return mqtt_client
+
+def send_payload(wifi_client, mqtt_client, times, timestamps):
+    
+    if not wifi_client.isconnected():
+        do_connect(wifi_client)
+        if wifi_client.isconnected():
+            set_time()
+            if timestamps[0] - 946684800 < 7200:
+                count = 0
+                for i in timestamps:
+                    timestamps[count] = 946684800 + utime.time() - i + 946684800
+
+    if wifi_client.isconnected():
+        rssi = wifi_client.status('rssi')
+        timestamp = 946684800 + utime.time()
+        if len(times) > 0:
+            count = 0
+            body = ''
+            for i in times:
+                if count == 0:
+                    body = f'{{"cycle_time":{i},"cycle_completed_timestamp":{timestamps[count]}}}'
+                else:
+                    body = f'{body},{{"cycle_time":{i},"cycle_completed_timestamp":{timestamps[count]}}}'
+                count += 1
+
+            try:
+                mqtt_client.publish(mqtt_data_topic, f'{{"data":[{body}],"timestamp":{timestamp},"rssi":{rssi}}}')
+            except OSError:
+                mqtt_client = connect_mqtt()
+                try:
+                    mqtt_client.publish(mqtt_data_topic, f'{{"data":[{body}],"timestamp":{timestamp},"rssi":{rssi}}}')
+                except OSError:
+                    ## If failed to send message just return for now
+                    return times, timestamps
+            return mqtt_client, [], []
+    else:
+        return mqtt_client,times, timestamps
