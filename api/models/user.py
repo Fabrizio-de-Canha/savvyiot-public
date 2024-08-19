0c1dea284d5575cad5c1162df2cb786885c1377d
Fabrizio-de-Canha <fabriziodecanha@gmail.com>
2024-08-19 20:57:11 +0200

    added frontend

--- api/models/user.py
+++ api/models/user.py
@@ -1,11 +1,16 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, text, DateTime, Text
 from ..auth_api.database import Base
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import JSONB, UUID, BYTEA
+from sqlalchemy.sql import func
 
 class User(Base):
     __tablename__ = "users"
     
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    name = Column('name', Text)
+    active = Column('active', Boolean, server_default=text("false"))
+    created_on = Column('created_on', DateTime(), server_default=func.now())
